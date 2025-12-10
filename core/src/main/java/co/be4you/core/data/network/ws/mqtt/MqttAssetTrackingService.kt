231da8c065fb5dffc4a93511489e42fba2132162
HrvojeCutvaric <111574558+HrvojeCutvaric@users.noreply.github.com>
2025-12-10 17:15:52 +0100

    Remove unused imports in MqttAssetTrackingService
    - Remove unused `android.util.Log.e` import.
    - Remove unused `org.eclipse.paho.client.mqttv3.MqttException` import.

--- core/src/main/java/co/be4you/core/data/network/ws/mqtt/MqttAssetTrackingService.kt
+++ core/src/main/java/co/be4you/core/data/network/ws/mqtt/MqttAssetTrackingService.kt
@@ -2,7 +2,6 @@ package co.be4you.core.data.network.ws.mqtt
 
 import android.os.Build
 import android.util.Log
-import android.util.Log.e
 import androidx.annotation.RequiresApi
 import co.be4you.core.data.network.services.AssetTrackingService
 import co.be4you.core.data.network.ws.mqtt.mappers.toAsset
@@ -24,7 +23,6 @@ import org.eclipse.paho.client.mqttv3.MqttAsyncClient
 import org.eclipse.paho.client.mqttv3.MqttCallback
 import org.eclipse.paho.client.mqttv3.MqttClient
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
-import org.eclipse.paho.client.mqttv3.MqttException
 import org.eclipse.paho.client.mqttv3.MqttMessage
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
