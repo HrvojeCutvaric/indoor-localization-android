1764f04dab6f57aee0893512bb11dfcbe08f49fe
HrvojeCutvaric <111574558+HrvojeCutvaric@users.noreply.github.com>
2026-02-02 02:19:06 +0100

    refactor(core): remove unused password length constant
    Removes `MIN_PASSWORD_LENGTH` from the `Constants` object as it is no longer used in the codebase.

--- core/src/main/java/co/be4you/core/domain/utils/Constants.kt
+++ core/src/main/java/co/be4you/core/domain/utils/Constants.kt
@@ -2,7 +2,6 @@ package co.be4you.core.domain.utils
 
 object Constants {
     const val BASE_URL = "http://13.49.49.83:5000"
-    const val MIN_PASSWORD_LENGTH = 6
     const val BEARER_TOKEN_PREFIX = "Bearer "
     const val AUTHORIZATION_HEADER = "Authorization"
 }
